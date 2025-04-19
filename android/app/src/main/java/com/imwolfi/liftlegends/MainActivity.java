06e492c2d79aa24e9643b1eacea49092836c3d8d
Ata Kamran <atakmrn14@gmail.com>
2025-04-19 14:01:14 +0330

    android build

---
+++ android/app/src/main/java/com/imwolfi/liftlegends/MainActivity.java
@@ -0,0 +1,5 @@
+package com.imwolfi.liftlegends;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.38f01ec7804044c48940e24559fbebf7',
-  appName: 'legend-lift-ai',
+  appId: 'com.imwolfi.liftlegends',
+  appName: 'Lift Legends',
   webDir: 'dist',
-  server: {
-    url: "https://38f01ec7-8040-44c4-8940-e24559fbebf7.lovableproject.com?forceHideBadge=true",
-    androidScheme: "https",
-    cleartext: false
-  },
   plugins: {
     SplashScreen: {
       launchShowDuration: 3000,
